a44610d11ce7efc6010633b5f7e18d27e6674540
MarcoHilliges <107026106+MarcoHilliges@users.noreply.github.com>
2022-08-23 11:04:59 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'familie-app-40ef7',
+    appId: '1:361525579602:web:257a428101faa40466f48b',
+    storageBucket: 'familie-app-40ef7.appspot.com',
+    apiKey: 'AIzaSyADbCuDAOfGZSu5MYdN53fnNV6zeYCTO4U',
+    authDomain: 'familie-app-40ef7.firebaseapp.com',
+    messagingSenderId: '361525579602',
+  },
   production: true
 };
